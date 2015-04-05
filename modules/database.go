d45e2255a56c1b8f8497719e08a6e9c2ba4b018c
Yohan Graterol <yohangraterol92@gmail.com>
2015-04-05 18:46:25 -0500

    Fix database.go, clean too

--- modules/database.go
+++ modules/database.go
@@ -1,74 +1,34 @@
-<<<<<<< HEAD
 package modules
-=======
-package modules
->>>>>>> 1b81e1dbb9c7e44c48bb68048eb9f950584f589a
-
 
 import (
-	"errors"
-	"labix.org/v2/mgo"
-	"time"
 	"log"
+	"time"
+
+	"labix.org/v2/mgo"
 )
 
-const {
+const (
 	MongoDBHost  = "example.mongo.com:35428"
-    AuthDatabase = "asio"
-	AuthUserName = "guest"
+	AuthDatabase = "asio"
+	AuthUsername = "guest"
 	AuthPassword = "youknowwhatis"
 	TestDatabase = "asio"
-}
+)
 
 func CreateDatabaseSession() {
-	mongoDBDialInfo := &mgo.DialInfo {
+	mongoDbDialInfo := &mgo.DialInfo{
 		Addrs:    []string{MongoDBHost},
 		Timeout:  60 * time.Second,
 		Database: AuthDatabase,
 		Username: AuthUsername,
-<<<<<<< HEAD
 		Password: AuthPassword,
-=======
-		Password: AuthPassword,
->>>>>>> 1b81e1dbb9c7e44c48bb68048eb9f950584f589a
 	}
 	// Create a session
 	mongoSession, err := mgo.DialWithInfo(mongoDbDialInfo)
 	if err != nil {
 		log.Fatalf("CreateSession: %s\n", err)
 	}
-<<<<<<< HEAD
 
 	// http://godoc.org/labix.org/v2/mgo#Session.SetMode
 	mongoSession.SetMode(mgo.Monotonic, true)
-
-	// Create a wait group to manage the goroutines.
-	var waitGroup sync.WaitGroup
-
-=======
-
-	// http://godoc.org/labix.org/v2/mgo#Session.SetMode
-	mongoSession.SetMode(mgo.Monotonic, true)
-
-	// Create a wait group to manage the goroutines.
-	var waitGroup sync.WaitGroup
-
->>>>>>> 1b81e1dbb9c7e44c48bb68048eb9f950584f589a
-	// Perform 10 concurrent queries against the database.
-	waitGroup.Add(10)
-	for query := 0; query < 10; query++ {
-		go RunQuery(query, &waitGroup, mongoSession)
-	}
-<<<<<<< HEAD
-
-	// Wait for all the queries to complete.
-	waitGroup.Wait()
-	log.Println("All Queries Completed")
 }
-=======
-
-	// Wait for all the queries to complete.
-	waitGroup.Wait()
-	log.Println("All Queries Completed")
-}
->>>>>>> 1b81e1dbb9c7e44c48bb68048eb9f950584f589a
