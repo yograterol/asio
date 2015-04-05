2df243a4048d2c45072d4037333d6b0eba3a3e1f
Yohan Graterol <yohangraterol92@gmail.com>
2015-04-05 17:00:58 -0500

    Added routes for single domain

--- asio.go
+++ asio.go
@@ -12,8 +12,20 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
 }
 
+func DummyMethod(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Dummy!")
+}
+
+// Create endpoints for all ASIO functions.
+// ASIO must support two versions, the previous and current release.
 func CreateRouters(router *httprouter.Router) {
 	router.GET("/", Index)
+	// *** v1 API REST ***
+	// Routes for single domain
+	router.GET("/v1/domain", DummyMethod)
+	router.GET("/v1/domain/:domain_id", DummyMethod)
+	router.POST("/v1/domain", DummyMethod) // Add an domain with settings
+	router.PUT("/v1/domain/:domain_id", DummyMethod)
 }
 
 func main() {
