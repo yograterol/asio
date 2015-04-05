13ad49a77d37966a7c27b26dc363606e9a8c6903
Yohan Graterol <yohangraterol92@gmail.com>
2015-04-05 15:41:38 -0500

    Added negroni and http router

--- asio.go
+++ asio.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/julienschmidt/httprouter"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func CreateRouters(router *httprouter.Router) {
+	router.GET("/", Index)
+}
+
 func main() {
-	fmt.Printf("Hello Asio :)")
+	router := httprouter.New()
+	n := negroni.Classic()
+	CreateRouters(&router)
+	n.UseHandler(router)
+	n.Run(":4000")
 }
