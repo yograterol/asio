66a13c33db34a2b8660f5167df57aa836c529d0a
Yohan Graterol <yohangraterol92@gmail.com>
2015-04-05 18:30:33 -0500

    Fix asio build

--- modules/database.go
+++ modules/database.go
@@ -1,4 +1,4 @@
-package modules 
+package modules
 
 
 import (
@@ -6,8 +6,7 @@ import (
 	"labix.org/v2/mgo"
 	"time"
 	"log"
-}
-
+)
 
 const {
 	MongoDBHost  = "example.mongo.com:35428"
@@ -23,26 +22,26 @@ func CreateDatabaseSession() {
 		Timeout:  60 * time.Second,
 		Database: AuthDatabase,
 		Username: AuthUsername,
-		Password: AuthPassword,     
+		Password: AuthPassword,
 	}
 	// Create a session
 	mongoSession, err := mgo.DialWithInfo(mongoDbDialInfo)
 	if err != nil {
 		log.Fatalf("CreateSession: %s\n", err)
 	}
-	
+
 	// http://godoc.org/labix.org/v2/mgo#Session.SetMode
 	mongoSession.SetMode(mgo.Monotonic, true)
-	
+
 	// Create a wait group to manage the goroutines.
 	var waitGroup sync.WaitGroup
- 
+
 	// Perform 10 concurrent queries against the database.
 	waitGroup.Add(10)
 	for query := 0; query < 10; query++ {
 		go RunQuery(query, &waitGroup, mongoSession)
 	}
- 
+
 	// Wait for all the queries to complete.
 	waitGroup.Wait()
 	log.Println("All Queries Completed")
