552cba7a1aeb744589b6eaf2a79278dc1dd4bdee
Eduardo Echeverria <echevemaster@gmail.com>
2015-04-05 00:19:54 -0500

    Added connection logic

--- models/domain.go
+++ models/domain.go
@@ -1,6 +1,20 @@
 // domain model
 package models
 
+import {
+	"labix.org/v2/mgo"
+	"time"
+	"log"
+}
+
+const {
+	MongoDBHost  = "example.mongo.com:35428"
+    AuthDatabase = "asio"
+	AuthUserName = "guest"
+	AuthPassword = "youknowwhatis"
+	TestDatabase = "asio"
+}
+
 type Domain struct {
 	Name string
 	URL  string
@@ -30,4 +44,35 @@ func NewDomainSetting(Domain *Domain, Interval, Retry, Timeout uint8) (DomainSet
 		return ds, nil
 	}
 	return DomainSetting{}, error.Error("The settings for the domain is not valid.")
+}
+
+func main() {
+	mongoDBDialInfo := &mgo.DialInfo {
+		Addrs:    []string{MongoDBHost},
+		Timeout:  60 * time.Second,
+		Database: AuthDatabase,
+		Username: AuthUsername,
+		Password: AuthPassword,     
+	}
+	// Create a session
+	mongoSession, err := mgo.DialWithInfo(mongoDbDialInfo)
+	if err != nil {
+		log.Fatalf("CreateSession: %s\n", err)
+	}
+	
+	// http://godoc.org/labix.org/v2/mgo#Session.SetMode
+	mongoSession.SetMode(mgo.Monotonic, true)
+	
+	// Create a wait group to manage the goroutines.
+	var waitGroup sync.WaitGroup
+ 
+	// Perform 10 concurrent queries against the database.
+	waitGroup.Add(10)
+	for query := 0; query < 10; query++ {
+		go RunQuery(query, &waitGroup, mongoSession)
+	}
+ 
+	// Wait for all the queries to complete.
+	waitGroup.Wait()
+	log.Println("All Queries Completed")
 }
