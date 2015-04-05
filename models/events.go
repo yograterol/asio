5e292b3e72ed947be97557dee6e9d24cbcc41708
Yohan Graterol <yohangraterol92@gmail.com>
2015-04-05 11:11:42 -0500

    Added services events

--- models/events.go
+++ models/events.go
@@ -1,14 +1,22 @@
 package models
 
-import (
-	"time"
-)
+import "time"
 
-type Event struct {
+type EventDomain struct {
 	DomainID    string    `bson:domain_id`
 	TypeID      string    `bson:type_id`
 	CreatedAt   time.Time `bson:created_at`
 	Description string    `bson:description`
+	// Source
+}
+
+// For the future
+type EventServices struct { // Services = Daemons
+	DaemonID    string    `bson:daemon_id`
+	TypeID      string    `bson:type_id`
+	CreatedAt   time.Time `bson:created_at`
+	Description string    `bson:description`
+	Environment string    `bson:env` // Shoul be a JSON field with all env data where run the daemon
 	// Source
 }
 
